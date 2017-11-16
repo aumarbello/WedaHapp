27d1a3c9111ebc83965984295a3b367985c6775c
Ahmed Umar <khattabu@gmail.com>
2017-11-16 17:06:37 +0100

    Completed weather view

---
+++ app/src/main/java/com/umar/ahmed/view/WeatherFragment.java
@@ -0,0 +1,138 @@
+package com.umar.ahmed.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v17.leanback.widget.HorizontalGridView;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.umar.ahmed.data.model.Main;
+import com.umar.ahmed.data.model.Weather;
+import com.umar.ahmed.data.model.WeatherDay;
+import com.umar.ahmed.data.model.WeatherItem;
+import com.umar.ahmed.weatherapp.R;
+
+import java.util.Calendar;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by ahmed on 11/15/17.
+ */
+
+public class WeatherFragment extends Fragment {
+    private static final String WeatherExtra = "Day_Extra";
+    private Unbinder unbinder;
+    private String cityName;
+    private String dayString;
+
+    public WeatherFragment(){
+
+    }
+
+    @BindView(R.id.weather_city)
+    TextView weatherCity;
+
+    @BindView(R.id.weather_degree)
+    TextView weatherDegree;
+
+    @BindView(R.id.weather_description)
+    TextView weatherDescription;
+
+    @BindView(R.id.weather_day)
+    TextView weatherDayString;
+
+    @BindView(R.id.weather_current_min_temp)
+    TextView currentMinTemp;
+
+    @BindView(R.id.weather_current_max_temp)
+    TextView currentMaxTemp;
+
+    @BindView(R.id.weather_day_list)
+    HorizontalGridView dayWeatherList;
+
+
+
+    public static WeatherFragment getInstance(WeatherDay day){
+        WeatherFragment fragment = new WeatherFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(WeatherExtra, day);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = View.inflate(getActivity(),
+                R.layout.weather_fragment, container);
+        unbinder = ButterKnife.bind(this, view);
+        WeatherDay weatherDay = (WeatherDay) getArguments()
+                .getSerializable(WeatherExtra);
+
+        if (weatherDay != null){
+            cityName = weatherDay.getCityName();
+            dayString = weatherDay.getDayString();
+            WeatherAdapter adapter = new WeatherAdapter(weatherDay.getStatsList(),
+                    this);
+            dayWeatherList.setAdapter(adapter);
+            dayWeatherList.setLayoutManager(new LinearLayoutManager(getActivity(),
+                    LinearLayoutManager.HORIZONTAL, false));
+            dayWeatherList.setHasFixedSize(true);
+
+            //TODO Load background image for view;
+
+            WeatherItem currentItem = getCurrentWeatherItem(weatherDay.getStatsList());
+            updateFragmentViews(currentItem);
+
+        }else
+            Log.d("WeatherFragment", "WeatherDay is null");
+        return view;
+    }
+
+    void updateFragmentViews(WeatherItem item) {
+        //weatherCity
+        weatherCity.setText(cityName);
+        //dayString
+        weatherDayString.setText(dayString);
+        //weatherDegree
+        Main weatherMain = item.getMain();
+        weatherDegree.setText(getString(R.string.empty_degree, weatherMain.getTemp()));
+        //weatherDescription
+        Weather firstWeather = item.getWeather().get(0);
+        weatherDescription.setText(firstWeather.getDescription());
+        //max and min temperature
+        currentMinTemp.setText(getString(R.string.empty_degree, weatherMain.getTempMin()));
+        currentMaxTemp.setText(getString(R.string.empty_degree, weatherMain.getTempMax()));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+    public WeatherItem getCurrentWeatherItem(List<WeatherItem> itemList) {
+        WeatherItem currentItem = itemList.get(0);
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        for (WeatherItem item : itemList){
+            Calendar weatherCalender = Calendar.getInstance();
+            weatherCalender.setTimeInMillis(item.getDt());
+            int weatherHour = weatherCalender.get(Calendar.HOUR_OF_DAY);
+            if (weatherHour ==  currentHour){
+                return item;
+            }
+        }
+        return currentItem;
+    }
+}
---
+++ app/src/main/java/com/umar/ahmed/view/WeatherPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.umar.ahmed.view;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.umar.ahmed.data.model.WeatherDay;
+
+import java.util.List;
+
+/**
+ * Created by ahmed on 11/15/17.
+ */
+
+class WeatherPagerAdapter extends FragmentStatePagerAdapter {
+    private List<WeatherDay> weatherDays;
+    WeatherPagerAdapter(FragmentManager fm, List<WeatherDay> weatherDays) {
+        super(fm);
+        this.weatherDays = weatherDays;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return WeatherFragment.getInstance(weatherDays.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return weatherDays.size();
+    }
+}
