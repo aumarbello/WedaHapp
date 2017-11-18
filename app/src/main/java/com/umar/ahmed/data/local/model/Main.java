4e7fe2536b0f487abf472972bdd79d21d6b03e52
Ahmed Umar <khattabu@gmail.com>
2017-11-18 15:32:40 +0100

    Corrected model data

--- app/src/main/java/com/umar/ahmed/data/local/model/Main.java
+++ app/src/main/java/com/umar/ahmed/data/local/model/Main.java
@@ -31,7 +31,7 @@ public class Main {
     private Integer humidity;
     @SerializedName("temp_kf")
     @Expose
-    private Integer tempKf;
+    private Double tempKf;
 
     public Double getTemp() {
         return temp;
@@ -89,11 +89,11 @@ public class Main {
         this.humidity = humidity;
     }
 
-    public Integer getTempKf() {
+    public Double getTempKf() {
         return tempKf;
     }
 
-    public void setTempKf(Integer tempKf) {
+    public void setTempKf(Double tempKf) {
         this.tempKf = tempKf;
     }
 }
